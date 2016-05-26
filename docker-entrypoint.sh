7aefb89c637c05ebfffb1d78847bd9c1f87b45b7
Eagle Chen <chygr1234@gmail.com>
2016-05-26 21:38:47 +0800

    correct permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,8 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ "$1" = 'elasticsearch' ]; then
+    # make sure the elasticserach directory and sub-directories after 'volume mount' are correct
+    chown -R es:es /usr/share/elasticsearch
     exec gosu es "$@"
 fi
 
